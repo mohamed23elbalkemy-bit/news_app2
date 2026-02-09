40d7515a42753aa94404ab47d49bfea70d7303dc
Mohamed Nasr <mohamed23elbalkemy@gmail.com>
2026-02-09 16:39:12 +0200

    Implement navigation screen

---
+++ lib/ui/screens/navigation/navigation_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'tabs/home_tab.dart';
+import 'tabs/categories_tab.dart';
+import 'tabs/profile_tab.dart';
+
+class NavigationScreen extends StatefulWidget {
+  const NavigationScreen({super.key});
+
+  @override
+  State<NavigationScreen> createState() => _NavigationScreenState();
+}
+
+class _NavigationScreenState extends State<NavigationScreen> {
+  int selectedIndex = 0;
+
+  final List<Widget> tabs = const [
+    HomeTab(),
+    CategoriesTab(),
+    ProfileTab(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: tabs[selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.grid_view),
+            label: "Categories",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/ui/screens/navigation/tabs/categories_tab.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CategoriesTab extends StatelessWidget {
+  const CategoriesTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "Categories Tab",
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
